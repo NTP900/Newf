39d10f60b25267b08604c7075b3902b94c200b9f
NTP900 <1md1seraj1@gmail.com>
2024-10-01 11:34:00 +0530

    Add files via upload

---
+++ jee_data.json.js
@@ -0,0 +1,20 @@
+[
+    { "minMarks": 300, "maxMarks": 281, "percentile": "100 - 99.999", "rank": "1 - 20" },
+    { "minMarks": 280, "maxMarks": 271, "percentile": "99.998 - 99.994", "rank": "21 - 80" },
+    { "minMarks": 270, "maxMarks": 251, "percentile": "99.994 - 99.96", "rank": "81 - 200" },
+    { "minMarks": 250, "maxMarks": 241, "percentile": "99.96 - 99.75", "rank": "201 - 500" },
+    { "minMarks": 240, "maxMarks": 221, "percentile": "99.75 - 99.4", "rank": "501 - 1000" },
+    { "minMarks": 220, "maxMarks": 201, "percentile": "99.4 - 99.1", "rank": "1001 - 2000" },
+    { "minMarks": 200, "maxMarks": 181, "percentile": "99.1 - 98.7", "rank": "2001 - 4000" },
+    { "minMarks": 180, "maxMarks": 161, "percentile": "98.7 - 98.3", "rank": "4001 - 6000" },
+    { "minMarks": 160, "maxMarks": 141, "percentile": "98.3 - 97.1", "rank": "6001 - 10000" },
+    { "minMarks": 140, "maxMarks": 121, "percentile": "97.1 - 96.2", "rank": "10001 - 15000" },
+    { "minMarks": 120, "maxMarks": 101, "percentile": "96.2 - 94.5", "rank": "15001 - 20000" },
+    { "minMarks": 100, "maxMarks": 81, "percentile": "94.5 - 93", "rank": "20001 - 30000" },
+    { "minMarks": 80, "maxMarks": 61, "percentile": "93 - 85", "rank": "30001 - 50000" },
+    { "minMarks": 60, "maxMarks": 41, "percentile": "85 - 73", "rank": "50001 - 70000" },
+    { "minMarks": 40, "maxMarks": 21, "percentile": "73 - 58", "rank": "70001 - 100000" },
+    { "minMarks": 20, "maxMarks": 0, "percentile": "Below 58", "rank": "100000+" }
+]
+
+
